8975d256384de1dba0524d10b769a224452ac540
devvadams <babaadamu2019@gmail.com>
2026-09-02 12:25:38 +0100

    primitives types, Object

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ let  name = "Adam";
 let firstName = "Abba";
 let lastName = "Umar";
 console.log(name);
+
+//const
+const interrestRate = 0.3;
+console.log(interrestRate);
+
+//Primitives Types 
+
+let names = "Adam"; //String Literal
+let age = 30;//Number Literal
+let isApproved = true;
+let middleName = undefined;
+let selectedColor = null;
+
+//Objects
+let persion = {
+    "name": "Baba",
+     age: 23,
+    "job": "Engineer"
+}
+//Dot Notation
+persion.name = "Adamu";
+//Bracket Notation
+let selection = "job";
+persion["job"] = "developer";
+console.log(persion.name);
+console.log(persion.job);
